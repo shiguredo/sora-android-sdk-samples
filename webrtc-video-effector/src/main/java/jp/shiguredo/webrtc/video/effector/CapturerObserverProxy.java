19393c964ce0780a9afe1916aaa3daa3a4c7873c
Shunichi Shinohara <shino@shiguredo.jp>
2018-03-28 11:27:52 +0900

    リファクタリング

--- webrtc-video-effector/src/main/java/jp/shiguredo/webrtc/video/effector/CapturerObserverProxy.java
+++ webrtc-video-effector/src/main/java/jp/shiguredo/webrtc/video/effector/CapturerObserverProxy.java
@@ -12,28 +12,22 @@ import org.webrtc.YuvHelper;
 import java.nio.ByteBuffer;
 
 public class CapturerObserverProxy implements VideoCapturer.CapturerObserver {
-
     public static final String TAG = CapturerObserverProxy.class.getSimpleName();
 
     private VideoCapturer.CapturerObserver originalObserver;
-    private SurfaceTextureHelper surfaceTextureHelper;
     private RTCVideoEffector videoEffector;
 
     public CapturerObserverProxy(final SurfaceTextureHelper surfaceTextureHelper,
                                  VideoCapturer.CapturerObserver observer,
                                  RTCVideoEffector effector) {
 
-        this.surfaceTextureHelper = surfaceTextureHelper;
         this.originalObserver = observer;
         this.videoEffector = effector;
 
-        final Handler handler = this.surfaceTextureHelper.getHandler();
-        ThreadUtils.invokeAtFrontUninterruptibly(handler, new Runnable() {
-            @Override
-            public void run() {
-                videoEffector.init(surfaceTextureHelper);
-            }
-        });
+        final Handler handler = surfaceTextureHelper.getHandler();
+        ThreadUtils.invokeAtFrontUninterruptibly(handler, () ->
+                videoEffector.init(surfaceTextureHelper)
+        );
     }
 
     @Override
@@ -49,50 +43,54 @@ public class CapturerObserverProxy implements VideoCapturer.CapturerObserver {
     @Override
     public void onFrameCaptured(VideoFrame frame) {
         if (this.videoEffector.needToProcessFrame()) {
-            final VideoFrame.I420Buffer buffer = frame.getBuffer().toI420();
+            // TODO(shino): libwebrtc 66.8.1, Android 7.0/Xperia Z4 では frame.getBuffer() は
+            // org.webrtc.NV21Buffer で実装されている。
+            // NV21Buffer には private data :: byte[] があるがアクセスは出来ない。
+            // VideoEffectorLogger.d(TAG, "frame.getBuffer: " + frame.getBuffer());
+
+            final VideoFrame.I420Buffer i420Buffer = frame.getBuffer().toI420();
+            // TODO: JavaI420Buffer は direct ByteBuffer を3つ別に持っている。
+            // それらが一直線かどうかは不明。実装次第だが toI420 でメモリコピーは不要。
+            // VideoEffectorLogger.d(TAG, "frame.getBuffer() = " + frame.getBuffer());
+            // VideoEffectorLogger.d(TAG, "i420Buffer = " + i420Buffer);
             frame.release();
 
-            final int height = buffer.getHeight();
-            final int width = buffer.getWidth();
-            final int strideY = buffer.getStrideY();
-            final int strideU = buffer.getStrideU();
-            final int strideV = buffer.getStrideV();
+            final int height = i420Buffer.getHeight();
+            final int width = i420Buffer.getWidth();
+            final int strideY = i420Buffer.getStrideY();
+            final int strideU = i420Buffer.getStrideU();
+            final int strideV = i420Buffer.getStrideV();
 
             final int chromaWidth = (width + 1) / 2;
             final int chromaHeight = (height + 1) / 2;
             final int dstSize = width * height + chromaWidth * chromaHeight * 2;
             ByteBuffer dst = ByteBuffer.allocateDirect(dstSize);
-            // TODO: libyuv に I420ToARGB があるので NV12 に変換する必要はないかもしれなが、
-            //       下回りの byte[] を取得する方法がわからない
-            YuvHelper.I420ToNV12(buffer.getDataY(), strideY, buffer.getDataU(), strideU,
-                    buffer.getDataV(), strideV, dst, width, height);
-            buffer.release();
+            // TODO: libyuv には I420ToARGB があるので NV12 に変換する必要はないかもしれない。
+            //       そもそも i420Buffer の byte[] が一直線の保証はないし、
+            //       下回りの byte[] を取得する方法も無い
+            YuvHelper.I420ToNV12(i420Buffer.getDataY(), strideY,
+                    i420Buffer.getDataU(), strideU,
+                    i420Buffer.getDataV(), strideV,
+                    dst, width, height);
+            i420Buffer.release();
 
-            byte[] filteredBytes =
+            byte[] effectedBytes =
                     this.videoEffector.processByteBufferFrame(dst.array(), width, height,
                             frame.getRotation(), frame.getTimestampNs());
 
             final int offsetY = 0;
             final int lengthY = strideY * height;
-            VideoEffectorLogger.d(TAG, "lengthY = " + lengthY);
             final int offsetU = offsetY + lengthY;
             final int lengthU = strideU * chromaHeight;
             final int offsetV = offsetU + lengthU;
             final int lengthV = strideV * chromaHeight;
 
-            final ByteBuffer dataY = ByteBuffer.allocateDirect(lengthY);
-            dataY.mark();
-            dataY.put(filteredBytes, offsetY, lengthY);
-            dataY.reset();
-            final ByteBuffer dataU = ByteBuffer.allocateDirect(lengthU);
-            dataU.mark();
-            dataU.put(filteredBytes, offsetU, lengthU);
-            dataU.reset();
-            final ByteBuffer dataV = ByteBuffer.allocateDirect(lengthV);
-            dataV.mark();
-            dataV.put(filteredBytes, offsetV, lengthV);
-            dataV.reset();
-
+            // JavaI420Buffer.wrap には direct ByteBuffer を渡す必要がある。
+            // VideoEffector#processByteBufferFrame の IN/OUT を ByteBuffer にすると
+            // ミスマッチが減りそう。
+            final ByteBuffer dataY = copy(effectedBytes, offsetY, lengthY);
+            final ByteBuffer dataU = copy(effectedBytes, offsetU, lengthU);
+            final ByteBuffer dataV = copy(effectedBytes, offsetV, lengthV);
             VideoFrame.I420Buffer filteredBuffer = JavaI420Buffer.wrap(
                     width, height,
                     dataY, strideY, dataU, strideU, dataV, strideV,
@@ -105,5 +103,13 @@ public class CapturerObserverProxy implements VideoCapturer.CapturerObserver {
         } else {
             this.originalObserver.onFrameCaptured(frame);
         }
+    }
+
+    private ByteBuffer copy(byte[] src, int offset, int length) {
+        ByteBuffer data = ByteBuffer.allocateDirect(length);
+        data.mark();
+        data.put(src, offset, length);
+        data.reset();
+        return data;
     }
 }
