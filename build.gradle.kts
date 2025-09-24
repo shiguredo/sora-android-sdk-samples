9254ca8d43d228a04a52d1f743d0062afaa4408e
t-miya <tatumiya008005@gmail.com>
2025-09-24 16:59:36 +0900

    kapt の依存を削除

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.android.maven) apply false
 }
